f6f1099be03b175e7d60627df7e6588c0487b747
dinothedinosaur <olya.yakovenko@bk.ru>
2018-04-08 16:08:00 +0700

    [database] Removed duplicate folders

--- voxforge_ru2/GO_cross.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /media/dino/DATA/ASR/voxforge_ru
-mkdir /media/dino/DATA/ASR/voxforge_ru/result_all_3gramm
-for i in {1..10}
-do
-	cp -a /media/dino/DATA/ASR/voxforge_ru/etc/$i/sphinx_train.cfg /media/dino/DATA/ASR/voxforge_ru/etc/sphinx_train.cfg && sphinxtrain run && tail -3 /media/dino/DATA/ASR/voxforge_ru/result/voxforge_ru.align >> /media/dino/DATA/ASR/voxforge_ru/result_all_3gramm/results
-done
--- voxforge_ru2/scripts/utils/__init__.py
+++
@@ -1,2 +0,0 @@
-__all__ = ['utils']
-from utils.utils import get_prompt, combine_texts
