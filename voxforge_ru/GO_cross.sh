5b3272704da0cc2a042b57bee051e47e26f8b63b
nsu-ai <36190785+nsu-ai@users.noreply.github.com>
2018-04-15 13:33:26 +0700

    Add files via upload

--- voxforge_ru/GO_cross.sh
+++ voxforge_ru/GO_cross.sh
@@ -2,5 +2,5 @@
 mkdir result_all_3gramm
 for i in {1..10}
 do
-	cp -a etc/$i/sphinx_train.cfg etc/sphinx_train.cfg && sphinxtrain run && tail -3 result/voxforge_ru.align >> result_all_3gramm/results
+	cp -a etc/$i/sphinx_train.cfg etc/sphinx_train.cfg && sphinxtrain -s decode run && cat result/voxforge_ru.align >> result_all_3gramm/results
 done
