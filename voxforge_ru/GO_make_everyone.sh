12df28685369da24165aa286e54fcf30453ecb5e
Ivan Bondarenko <iv.bondarenko@2gis.ru>
2018-05-01 21:11:11 +0700

    Prepared sphinx_train.cfg has been removed from everywhere.

--- voxforge_ru/GO_make_everyone.sh
+++ voxforge_ru/GO_make_everyone.sh
@@ -11,7 +11,7 @@ python3 dict2pickle.py
 for i in {1..10}
 do
 	cd $i
-        perl -i -p -e "s/CFG_PART = $default_jobs/CFG_PART = $NJOBS/" sphinx_train.cfg
+        perl -i -p -e "s/CFG_NPART = $default_jobs/CFG_NPART = $NJOBS/" sphinx_train.cfg
 	python3 everything_maker.py
 	ngram-count -text corpus_train -order 3 -write voxforge_ru.count -unk
 	ngram-count -lm voxforge_ru.lm -order 3 -read voxforge_ru.count -kndiscount1 -kndiscount2 -kndiscount3
