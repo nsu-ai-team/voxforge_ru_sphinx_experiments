7fa42a7fa09d98bd35599ec6e0a43faa422de61c
Ivan Bondarenko <iv.bondarenko@2gis.ru>
2018-05-01 21:23:33 +0700

    Prepared sphinx_train.cfg has been removed from everywhere. Now sphinx_train.cfg is created on-the-fly.

--- voxforge_ru/GO_make_everyone.sh
+++ voxforge_ru/GO_make_everyone.sh
@@ -6,12 +6,14 @@ NJOBS=${1:-$default_jobs}
 
 sphinxtrain -t voxforge_ru setup
 cd etc
+perl -i -p -e "s/CFG_NPART = $default_jobs/CFG_NPART = $NJOBS/" sphinx_train.cfg
+perl -i -p -e "s/CFG_QUEUE_TYPE = \"Queue\"/CFG_QUEUE_TYPE = \"Queue::POSIX\"/" sphinx_train.cfg
 python3 dict2pickle.py
 
 for i in {1..10}
 do
 	cd $i
-        perl -i -p -e "s/CFG_NPART = $default_jobs/CFG_NPART = $NJOBS/" sphinx_train.cfg
+        cp ../sphinx_train.cfg sphinx_train.cfg
 	python3 everything_maker.py
 	ngram-count -text corpus_train -order 3 -write voxforge_ru.count -unk
 	ngram-count -lm voxforge_ru.lm -order 3 -read voxforge_ru.count -kndiscount1 -kndiscount2 -kndiscount3
