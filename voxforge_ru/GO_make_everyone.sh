f424c51b9a25d47734898e9fc77be2bc6dfb20a0
Ivan Bondarenko <iv.bondarenko@2gis.ru>
2018-05-01 19:13:23 +0700

    Multiprocessing has been added in .

--- voxforge_ru/GO_make_everyone.sh
+++ voxforge_ru/GO_make_everyone.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -e
+
+default_jobs=1
+NJOBS=${1:-$default_jobs}
 
 sphinxtrain -t voxforge_ru setup
 cd etc
@@ -7,6 +11,7 @@ python3 dict2pickle.py
 for i in {1..10}
 do
 	cd $i
+        perl -i -p -e "s/CFG_PART = $default_jobs/CFG_PART = $NJOBS/" sphinx_train.cfg
 	python3 everything_maker.py
 	ngram-count -text corpus_train -order 3 -write voxforge_ru.count -unk
 	ngram-count -lm voxforge_ru.lm -order 3 -read voxforge_ru.count -kndiscount1 -kndiscount2 -kndiscount3
