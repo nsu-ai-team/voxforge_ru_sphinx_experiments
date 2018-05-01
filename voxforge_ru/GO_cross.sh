a6648f84c22c0bdebe39aa71e6603d82387c47b2
Ivan Bondarenko <iv.bondarenko@2gis.ru>
2018-05-01 21:34:05 +0700

    Bug in GO_cross.sh has been fixed.

--- voxforge_ru/GO_cross.sh
+++ voxforge_ru/GO_cross.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -e
+
 mkdir result_all_3gramm
 for i in {1..10}
 do
-	cp -a etc/$i/sphinx_train.cfg etc/sphinx_train.cfg && cp -a etc/&i/voxforge_ru.dic etc/voxforge_ru.dic && sphinxtrain run && tail -3 result/voxforge_ru.align >> result_all_3gramm/results
+	cp -a "etc/$i/sphinx_train.cfg" etc/sphinx_train.cfg && cp -a "etc/$i/voxforge_ru.dic" etc/voxforge_ru.dic && sphinxtrain run && tail -3 result/voxforge_ru.align >> result_all_3gramm/results
 done
